00bf00468c6a35ece45c2373127a303f7807205d
Pepsi-21 <68282828+Pepsi-21@users.noreply.github.com>
2020-07-14 21:36:21 +0700

    Add files via upload

---
+++ FirstHomeWork.cpp
@@ -0,0 +1,13 @@
+#include<iostream>;
+using namespace std;
+void main();
+{
+    cout<<"    #    "<<endl;
+    cout<<"  #   #  "<<endl;
+    cout<<" #     # "<<endl;
+    cout<<"#       #"<<endl;
+    cout<<"#########"<<endl;
+    cout<<"#       #"<<endl;
+    cunt<<"Thanakorn Mongsawai\n";
+    system("pause");
+}
